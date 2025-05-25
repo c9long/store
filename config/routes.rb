9bb46671e9cc8073cf4e05a16e53932846e6a27a
c9long <c9long@uwaterloo.ca>
2025-05-24 23:55:30 -0400

    16.2 add subscribers to products

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resource :session
   resources :passwords, param: :token
   root "products#index"
-  resources :products
+  resources :products do
+    resources :subscribers, only: [:create]
+  end
 end
